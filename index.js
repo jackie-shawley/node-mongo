f2005813651fcf4720ec33ccf1b5c51b801f8334
jackie-shawley <jackiekopco@aol.com>
2021-09-02 12:54:16 -0400

    Node Callback Hell and Promises

--- index.js
+++ index.js
@@ -1,53 +1,51 @@
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert').strict;
 const dboper = require('./operations'); //dboper is short for database operations
 
 const url = 'mongodb://localhost:27017/';  //common mongodb protocol 
 const dbname = 'nucampsite';
 
 //use the MongoClient.connect method to connect to the MongoDB server
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-
-    assert.strictEqual(err, null);  //allows us to check if the first value is true, the second argument is the second value that we're checking against (are the two values strictly equal?); If the actual and expected arguments match, the code will just continue. If it fails, it will throw an error and terminate the entire application and log the error to the console. 
-        //assert is a core method
+MongoClient.connect(url, { useUnifiedTopology: true }).then(client => {
+    
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
 
+    db.dropCollection('campsites') //the result here is what is returned from the previous promise
+    .then(result => {
+        console.log('Dropped Collection: ', result);
+    })
+    .catch(err => console.log('No collection to drop')); //this error will not stop the code from running; the code will continue running even if there is an error here
 
-    //drop means delete. be careful when using drop, because deleted items can be hard to recover
-    db.dropCollection('campsites', (err, result) => {
-        assert.strictEqual(err, null);
-        console.log('Dropped Collection', result);
+    dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test" }, 'campsites')
+    .then(result => {
+        console.log('Insert Document:', result.ops);
 
-        dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test" },
-            'campsites', result => {
-                console.log('Insert Document: ', result.ops);  //this is the callback used in the operations insertDocument method
+        return dboper.findDocuments(db, 'campsites'); //we want to return a promise here, so we use the return keyword and do not have a callback in the parameter list
+    })
+    .then(docs => {
+        console.log('Found Documents:', docs);
 
-                dboper.findDocuments(db, 'campsites', docs => {
-                    console.log('Found Documents: ', docs);
+        return dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" }, { description: "Updated Test Description" }, 'campsites');
+    })
+    .then(result => {
+        console.log('Updated Document Count: ', result.result.nModified);
 
-                    dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" }, 
-                        { description: "Updated Test Description" }, 'campsites', 
-                        result => {
-                            console.log('Updated Document Count: ', result.result.nModified);
+        return dboper.findDocuments(db, 'campsites');
+    })
+    .then(docs => {
+        console.log('Found Documents:', docs);
 
-                            dboper.findDocuments(db, 'campsites', docs => {
-                                console.log('Found Documents: ', docs);
+        return dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" }, 'campsites');
+    })
+    .then(result => {
+        console.log('Deleted Document Count:', result.deletedCount);
 
-                                dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
-                                    'campsites', result => {
-                                        console.log('Deleted Document Count: ', result.deletedCount);
-
-                                        client.close();
-                                    }
-                                );
-                            });
-                        }
-                    );
-                });
-            });
-        });
+        return client.close();
+    })
+    .catch(err => {
+        console.log(err);
+        client.close();
     });
-
-
+})
+.catch(err => console.log(err));
--- operations.js
+++ operations.js
@@ -1,35 +1,21 @@
-const assert = require('assert').strict;
-
-exports.insertDocument = (db, document, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    coll.insertOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.insertOne(document); //because we've removed the callback, it will automatically return a promise, so we added the return keyword in front of the method call
 };
 
-exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
     const coll = db.collection(collection); 
-    coll.find().toArray((err, docs) => {
-        assert.strictEqual(err, null);
-        callback(docs);
-    });
+    return coll.find({}).toArray();
 };
 
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.deleteOne(document);
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
     const coll = db.collection(collection);
-    coll.updateOne(document, { $set: update }, null, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.updateOne(document, { $set: update }, null);
 };
 
 //the update operator is { $set: update } which is an object that MongoDB will see and understand that we want to overwrite information
